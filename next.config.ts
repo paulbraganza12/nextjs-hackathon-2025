c3f13d326a4e4deee6319683f9d21f8d8199e751
Peter Braganza <peter082796@gmail.com>
2025-04-12 23:27:34 +0100

    feat: property card

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["a0.muscache.com", "placehold.co"], // Add the hostname here
+  },
 };
 
 export default nextConfig;
